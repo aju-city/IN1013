1a50dd6fa005f63b810c2af4c5153cc0fe266dc5
aju-city <Arjun.Bector@city.ac.uk>
2024-10-27 16:39:20 +0000

    Update task1.sql

--- HW 4/task1.sql
+++ HW 4/task1.sql
@@ -6,20 +6,20 @@
 
 -- Up to the next CUTOFF POINT should be in task1.sql
 
-??? petPet (
-  petname ???,
-  owner ???,
-  species ???,
-  gender ???,
-  birth ???,
-  death ???
+CREATE TABLE petPet (
+  petname VARCHAR(20) PRIMARY KEY NOT NULL,
+  owner VARCHAR(45) NOT NULL,
+  species ENUM('M', 'F') NOT NULL,
+  gender VARCHAR(15),
+  birth DATE,
+  death DATE
 );
 
-??? petEvent (
-  petname ???,
-  eventdate ???,
-  eventtype ???,
-  remark ???,
-  FOREIGN KEY ???,
-  PRIMARY KEY ???
+CREATE TABLE petEvent (
+  petname VARCHAR(20) NOT NULL,
+  eventdate DATE NOT NULL,
+  eventtype VARCHAR(15) NOT NULL,
+  remark VARCHAR(255),
+  FOREIGN KEY (petname) REFERENCES petPet(petname),
+  PRIMARY KEY (petname, eventdate)
 );
