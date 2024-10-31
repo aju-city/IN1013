d7404b9cf25b51b54ee3849cb328d9d7f6dfeb45
aju-city <Arjun.Bector@city.ac.uk>
2024-10-31 17:17:08 +0000

    Update task1.sql

--- HW 5/task1.sql
+++ HW 5/task1.sql
@@ -1,12 +1,17 @@
 -- 1
 SELECT * FROM restBill WHERE cust_name = 'Bob Crow';
 
+--2
 SELECT DISTINCT * FROM restBill WHERE cust_name LIKE "%smith" ORDER BY cust_name DESC;
 
+--3
 SELECT DISTINCT cust_name FROM restBill WHERE cust_name LIKE "% C%";
 
+--4
 SELECT * FROM restStaff WHERE headwaiter IS NOT NULL;
 
+--5
 SELECT * FROM restBill WHERE bill_date LIKE '1602%';
 
+--6
 SELECT DISTINCT * FROM restBill WHERE bill_date LIKE '15%' ORDER BY bill_date ASC;
